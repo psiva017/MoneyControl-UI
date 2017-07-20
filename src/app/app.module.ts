20ddee9506f1e3627c8c96e3b1e9edf5438f5cd9
Siva Pasupathi <sivap@revature.com>
2017-07-20 10:23:49 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    LoginComponent,
+    SignupComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
